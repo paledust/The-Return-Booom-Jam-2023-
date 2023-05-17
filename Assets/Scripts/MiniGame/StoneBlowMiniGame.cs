962c9d2c260d57a9be36cceb970cf6a5e7e0f77f
paledust <paledust@hotmail.com>
2023-05-17 23:58:52 +0800

    part I done

--- Assets/Scripts/MiniGame/StoneBlowMiniGame.cs
+++ Assets/Scripts/MiniGame/StoneBlowMiniGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class StoneBlowMiniGame : MiniGameBasic
 {
@@ -8,30 +9,47 @@ public class StoneBlowMiniGame : MiniGameBasic
     [SerializeField] private Vector2 torqueRange;
     [SerializeField] private Vector2 forceRange;
     [SerializeField] private Vector2 timeRange;
-[Header("Sand Particles")]
+    [SerializeField] private Vector2 spawnIntersectionRange;
+[Header("Other stone")]
+    [SerializeField] private StoneDebrisGenerator stoneGenerator;
+[Header("End MiniGame")]
     [SerializeField] private ParticleSystem sandParticles;
+    [SerializeField] private float directorStartDelay;
+    [SerializeField] private PlayableDirector director;
     private float totalCount;
     protected override void Initialize(){
         base.Initialize();
         totalCount = stoneDebris.Count;
     }
     protected override void OnKeyPressed(UnityEngine.InputSystem.Key keyPressed){
+        float debrisProgress = 1-stoneDebris.Count/(totalCount-1);
+
+        if(!stoneGenerator.gameObject.activeSelf) stoneGenerator.gameObject.SetActive(true);
+        stoneGenerator.spawnIntersection = Mathf.Lerp(spawnIntersectionRange.x, spawnIntersectionRange.y, debrisProgress);
+
         int index = Random.Range(0, stoneDebris.Count);
         ConstantForce debri = stoneDebris[index];
 
         stoneDebris.RemoveAt(index);
-        StartCoroutine(coroutineForceOnRock(debri, Mathf.Lerp(timeRange.x, timeRange.y, 1-stoneDebris.Count/(totalCount-1))));
+        StartCoroutine(coroutineForceOnRock(debri, Mathf.Lerp(timeRange.x, timeRange.y, debrisProgress)));
 
         if(stoneDebris.Count == 0){
             sandParticles.Play(true);
+            StartCoroutine(coroutineEndMiniGame());
             EventHandler.Call_OnEndMiniGame(this);
         }
     }
+    public void StopSandStorm()=>sandParticles.Stop(true);
+    public void StopStoneGenerate()=>stoneGenerator.StopStoneSpawn();
     IEnumerator coroutineForceOnRock(ConstantForce cforce, float time){
         for(float t=0; t<1; t+=Time.deltaTime/time){
             cforce.torque = Vector3.right * Mathf.Lerp(torqueRange.x, torqueRange.y, t);
             cforce.force  = Vector3.forward * Mathf.Lerp(forceRange.x, forceRange.y, t);
             yield return null;
         }
+    }
+    IEnumerator coroutineEndMiniGame(){
+        yield return new WaitForSeconds(directorStartDelay);
+        director.Play();
     }
 }
--- Assets/Scripts/StoneDebrisGenerator.cs
+++ Assets/Scripts/StoneDebrisGenerator.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class StoneDebrisGenerator : MonoBehaviour
 {
+    public float spawnIntersection = 1;
     [SerializeField] private GameObject[] stonePrefabs;
     [SerializeField] private float spawnSize = 1;
-    [SerializeField] private float spawnIntersection = 1;
     [SerializeField] private int maxStoneCount = 50;
     [SerializeField] private Rect spawnZone;
     private List<ConstantForce> stoneDebris;
     private float stoneTime = 0;
-    private int stoneIndex = 0;
+    private bool stopSpawn = false;
+    [SerializeField, ShowOnly]private int activeStone = 0;
     void Start(){
         stoneDebris = new List<ConstantForce>();
         for(int i=0;i<maxStoneCount;i++){
@@ -25,7 +26,7 @@ public class StoneDebrisGenerator : MonoBehaviour
         }
     }
     void Update(){
-        if(Time.time>stoneTime+spawnIntersection){
+        if(Time.time>stoneTime+spawnIntersection && !stopSpawn){
             stoneTime = Time.time;
             var stoneTrans = stoneDebris.Find(x=>!x.gameObject.activeSelf).transform;
         //Active one stones from pool
@@ -33,13 +34,31 @@ public class StoneDebrisGenerator : MonoBehaviour
             stoneTrans.rotation = Quaternion.Euler(Random.Range(0f,360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
             stoneTrans.localScale = Vector3.one * spawnSize * Random.Range(0.5f, 1.5f);
             stoneTrans.gameObject.SetActive(true);
+            activeStone ++;
+        }
+        for(int i=0;i<maxStoneCount;i++){
+            if(stoneDebris[i].transform.position.z>transform.position.z+spawnZone.height){
+                stoneDebris[i].gameObject.SetActive(false);
+                activeStone --;
+            } 
         }
     }
-
-    private Color trans_green = new Color(0,1,0,0.2f);
+    public void StopStoneSpawn() => StartCoroutine(coroutineStopStoneSpawn());
+    IEnumerator coroutineStopStoneSpawn(){
+        stopSpawn = true;
+        while(activeStone>0){
+            yield return null;
+        }
+        
+        for(int i=0; i<maxStoneCount; i++){
+            Destroy(stoneDebris[i].gameObject);
+        }
+        stoneDebris.Clear();
+        gameObject.SetActive(false);
+    }
     void OnDrawGizmosSelected(){
         Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.color  = trans_green;
+        Gizmos.color  = new Color(0,1,0,0.2f);
         Vector3 center= new Vector3(spawnZone.xMin, 0, spawnZone.center.y);
         Vector3 size= new Vector3(spawnZone.size.x, 0, spawnZone.size.y);
         Gizmos.DrawCube(center, size);
