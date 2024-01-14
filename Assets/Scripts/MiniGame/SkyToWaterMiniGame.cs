44bea30b427e2f39de2c0ea5a1664a4dda63104a
paledust <paledust@hotmail.com>
2024-01-14 17:39:09 +0800

    end mini game

--- Assets/Scripts/MiniGame/SkyToWaterMiniGame.cs
+++ Assets/Scripts/MiniGame/SkyToWaterMiniGame.cs
@@ -12,20 +12,28 @@ public class SkyToWaterMiniGame : MiniGameBasic
     [SerializeField] private ParticleSystem fishParticles;
     [SerializeField] private ParticleSystem rippleParticles;
 [Header("End")]
+    [SerializeField, Range(0, 1)] private float TriggerPercentage = 0.5f;
     [SerializeField] private MeshRenderer skyRenderer;
     [SerializeField] private float fadeOutDuration = 5;
     private Vector2[] spawnPos;
+    private bool[] triggerArray;
+    private int counter=0;
+
     private const int ROLL = Service.ROLL;
     private const int LINE = Service.LINE;
+
     protected override void Initialize()
     {
         base.Initialize();
 
+        counter = 0;
         spawnPos = new Vector2[ROLL*LINE];
+        triggerArray = new bool[ROLL*LINE];
         
         skyObject.SetActive(true);
         for(int y=0; y<ROLL; y++){
             for(int x=0;x<LINE;x++){
+                triggerArray[y*LINE+x] = false;
                 spawnPos[y*LINE+x] = new Vector2(x/(LINE-1.0f)*fishRect.width, -y/(ROLL-1.0f)*fishRect.height)+new Vector2(-0.5f*fishRect.width,0.5f*fishRect.height);
             }
         }
@@ -36,7 +44,10 @@ public class SkyToWaterMiniGame : MiniGameBasic
 
         Vector2Int coordinate = keyMatrix.GetCoordinateFromKey(keyPressed);
         Vector3 location;
-        location = spawnPos[coordinate.y*LINE + coordinate.x];
+        int index = coordinate.y*LINE + coordinate.x;
+
+
+        location = spawnPos[index];
         location.z = location.y;
         location.y = fishParticles.transform.position.y;
 
@@ -47,11 +58,20 @@ public class SkyToWaterMiniGame : MiniGameBasic
         location.y = rippleParticles.transform.position.y;
         rippleParticles.transform.position = location;
         rippleParticles.Play(true);
+
+        if(!triggerArray[index]){
+            triggerArray[index] = true;
+            counter ++;
+        }
+
+        if(counter >= TriggerPercentage*(LINE*ROLL)){
+            EventHandler.OnEndMiniGame(this);
+            EventHandler.OnNextMiniGame();
+        }
     }
     protected override void CleanUp()
     {
         base.CleanUp();
-
         StartCoroutine(coroutineFadeOutSky(fadeOutDuration));
     }
     IEnumerator coroutineFadeOutSky(float duration){
